b3a35ab306d9e088b083e1cb75dcf7d5180360d4
Arya Sinha <aryasinhacr7@gmail.com>
2025-11-24 22:28:24 +0530

    Dummy

---
+++ app.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+root = Tk()
+root.title("Complaint System - Prototype")
+root.geometry("400x300")
+
+label = Label(root, text="Welcome to Complaint Tracking System", font=("Arial", 12))
+label.pack(pady=50)
+
+root.mainloop()
